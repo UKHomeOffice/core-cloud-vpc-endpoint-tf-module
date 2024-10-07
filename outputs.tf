bde49b642ab71b6c66fc3f2897d31c5a606d3bc3
Johnny Che <johnny.che@capgemini.com>
2024-10-07 15:57:10 +0100

    CCL-663: add VPCe DNS entry output

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "vpce_endpoint_arn" {
   value = aws_vpc_endpoint.this.arn
 }
 
+output "vpce_endpoint_dns" {
+  value = aws_vpc_endpoint.this.dns_entry
+}
+
 output "custom_r53_phz_id" {
   value = aws_route53_zone.private.*.id
 }
